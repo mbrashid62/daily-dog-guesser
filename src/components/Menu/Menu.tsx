451975fe86c34a6b0621827949acb62a541651f0
Bill Rashid <bill.rashid12@gmail.com>
2024-12-25 19:08:31 -0500

    cleanup styles, spacing

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -52,17 +52,19 @@ function mapAuthUserToInternalUser(authUser: unknown): User {
   };
 }
 
-const GoBackNavLinkItem = () => {
+const GoBackNavLinkItem = ({ showBack }: { showBack: boolean }) => {
+  if (!showBack) {
+    return null;
+  }
+
   return (
-    <li>
-      <Link className="go-back-link nav-link fourth-color" to="/">
-        <img
-          src="/back-arrow.png"
-          style={{ width: 20, height: 20, paddingRight: 16 }}
-        />
-        Back
-      </Link>
-    </li>
+    <Link className="go-back-link nav-link fourth-color" to="/">
+      <img
+        src="/back-arrow.png"
+        style={{ width: 20, height: 20, paddingRight: 16 }}
+      />
+      Back
+    </Link>
   );
 };
 
@@ -73,7 +75,7 @@ const HelpLinkItem = ({
 }) => {
   const [showHelp, setShowHelp] = showHelpState;
   return (
-    <li style={{ display: "inline-flex" }}>
+    <div style={{ display: "inline-flex" }}>
       <img
         alt="Help Icon"
         src="/question.png"
@@ -81,17 +83,41 @@ const HelpLinkItem = ({
         style={{ width: 20, height: 20, cursor: "pointer" }}
       />
       <HelpModal showHelp={showHelp} setShowHelp={setShowHelp} />
-    </li>
+    </div>
   );
 };
 
-const ProfilePhotoLinkItem = ({ url }: { url: string }) => {
+const ProfilePhotoLinkItem = ({ url }: { url: string | null }) => {
+  if (!url) {
+    return null;
+  }
+
   return (
-    <li className="profile-photo-container">
+    <div className="profile-photo-container">
       <Link to="/">
         <img alt="Profile Photo." src={url} />
       </Link>
-    </li>
+    </div>
+  );
+};
+
+export const MenuOptions = ({ children }: { children: React.ReactElement }) => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <div className="more-options-container">
+      <div
+        className="more-options-trigger"
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <img alt="More Options Icon" src="/menu.png" />
+      </div>
+      <div
+        className={`more-options-list-container ${openMenu ? "open" : "closed"}`}
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
@@ -151,7 +177,7 @@ export const Menu = () => {
     return (
       <nav className="nav-container">
         <ul className="nav-links">
-          {location.pathname !== "/" && <GoBackNavLinkItem />}
+          <GoBackNavLinkItem showBack={location.pathname !== "/"} />
           <HelpLinkItem showHelpState={[showHelp, setShowHelp]} />
         </ul>
         <ul className="nav-links">
@@ -170,21 +196,23 @@ export const Menu = () => {
   // 4. profile icon
   return (
     <nav className="nav-container">
-      <ul className="nav-links">
-        {location.pathname !== "/" && <GoBackNavLinkItem />}
+      <div className="nav-links">
+        <GoBackNavLinkItem showBack={location.pathname !== "/"} />
         <HelpLinkItem showHelpState={[showHelp, setShowHelp]} />
-      </ul>
-      <ul className="nav-links">
-        <li>
-          <Link className="nav-link fourth-color" to="Privacy">
-            Account
-          </Link>
-        </li>
-        <li className="nav-link fourth-color" onClick={handleLogout}>
-          Sign out
-        </li>
-        {user.photoURL && <ProfilePhotoLinkItem url={user.photoURL} />}
-      </ul>
+      </div>
+      <div className="right-col">
+        <MenuOptions>
+          <div className="menu-links">
+            <div className="menu-link-item">
+              <Link to="Privacy">Account</Link>
+            </div>
+            <div className="menu-link-item" onClick={handleLogout}>
+              <a href="javascript:void(0);">Sign out</a>
+            </div>
+          </div>
+        </MenuOptions>
+        <ProfilePhotoLinkItem url={user.photoURL} />
+      </div>
     </nav>
   );
 };
